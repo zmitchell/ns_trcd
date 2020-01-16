fd6059e1ceeba39a40cfa82de2c0da60d6ef7c3d
Zach Mitchell <zmitchell@fastmail.com>
2020-01-16 18:12:45 -0500

    feat: Generate data in experiment worker thread

--- ns_trcd/__main__.py
+++ ns_trcd/__main__.py
@@ -1,14 +1,11 @@
 from PySide2.QtWidgets import QApplication
 import sys
-from .oscilloscope import Oscilloscope
 from .ui import MainWindow
 
 
-# app = QApplication(sys.argv)
+app = QApplication(sys.argv)
 
-# window = MainWindow()
-# window.show()
+window = MainWindow()
+window.show()
 
-# sys.exit(app.exec_())
-
-scope = Oscilloscope()
+sys.exit(app.exec_())
